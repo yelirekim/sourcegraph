4821c093b95a33f0f6f0b02d88a641ce1700591d
Quinn Slack <qslack@qslack.com>
2019-03-16 03:05:04 -0700

    WIP checks router changes

--- cmd/frontend/internal/app/ui/router.go
+++ cmd/frontend/internal/app/ui/router.go
@@ -42,6 +42,7 @@ const (
 	routeRepoCompare    = "repo-compare"
 	routeRepoStats      = "repo-stats"
 	routeRepoGraph      = "repo-graph"
+	routeRepoChecks     = "repo-checks"
 	routeThreads        = "threads"
 	routeTree           = "tree"
 	routeBlob           = "blob"
@@ -155,6 +156,7 @@ func newRouter() *mux.Router {
 
 	repoRev.PathPrefix("/commits").Methods("GET").Name(routeRepoCommits)
 	repoRev.PathPrefix("/graph").Methods("GET").Name(routeRepoGraph)
+	repoRev.PathPrefix("/checks").Methods("GET").Name(routeRepoChecks)
 
 	// blob
 	repoRev.Path("/blob{Path:.*}").Methods("GET").Name(routeBlob)
@@ -206,6 +208,7 @@ func initRouter() {
 	router.Get(routeRepoCompare).Handler(handler(serveBasicPageString("Compare - Sourcegraph")))
 	router.Get(routeRepoStats).Handler(handler(serveBasicPageString("Stats - Sourcegraph")))
 	router.Get(routeRepoGraph).Handler(handler(serveBasicPageString("Repository graph - Sourcegraph")))
+	router.Get(routeRepoChecks).Handler(handler(serveBasicPageString("Checks - Sourcegraph")))
 	router.Get(routeSearchScope).Handler(handler(serveBasicPageString("Search scope - Sourcegraph")))
 	router.Get(routeSurvey).Handler(handler(serveBasicPageString("Survey - Sourcegraph")))
 	router.Get(routeSurveyScore).Handler(handler(serveBasicPageString("Survey - Sourcegraph")))
